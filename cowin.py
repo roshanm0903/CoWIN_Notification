217fac4b9d413823f07e31600c08da79b746fcb6
roshanm0903 <roshanm0903@gmail.com>
2021-05-14 22:44:31 +0530

    new

---
+++ cowin.py
@@ -0,0 +1,44 @@
+from urllib.request import Request, urlopen
+import json
+import re
+import time
+import datetime
+from win10toast import ToastNotifier
+
+
+def get_data():
+    current_time = datetime.datetime.now() 
+    print("running... ",current_time)
+    today = str(current_time.day) +"-"+ str(current_time.month)  +"-"+ str(current_time.year) 
+    first_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=571&date="+today
+
+    # print(first_url)
+    data = {}
+    
+    # time.sleep(1)
+   
+    req = Request(first_url, headers={'User-Agent': 'Mozilla/5.0'} )
+    inp = urlopen(req)
+    resp = json.load(inp)
+
+    for i in resp['centers']:
+        if i['fee_type'] == "Paid":
+            for j in i['sessions']:
+                if j["min_age_limit"] == 18:
+                    # data[i['name']] [j["date"]] = j["available_capacity"]
+                    # print( j["date"] , j["available_capacity"] )
+                    if j["available_capacity"] > 0:
+                        # print("Slots Avaialble")
+                        print(j["date"] +" "+ i["name"]  +" "+ str(j["available_capacity"]) )
+                        toast.show_toast("Notification","Vacant",duration=4)
+                        
+
+        
+    return False
+
+toast = ToastNotifier()
+
+while True:
+    get_data()
+    time.sleep(5)
+
